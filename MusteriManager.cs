bf3ea0600e84d6f33e1bbc7fa9ce7a0a21231604
aleynailhannn@gmail.com <aleynailhannn@gmail.com>
2021-02-01 13:57:09 +0300

    Proje dosyası ekle.

---
+++ MusteriManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musterii)
+        {
+            Console.WriteLine("Musteri " + musterii.Adi + musterii.Soyadi + " eklendi");
+            Console.WriteLine("-------------------------------------");
+        }
+
+        public void Listele(Musteri[] musteriler)
+            {
+
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Adi + " " + musteri.Soyadi);
+            }
+            Console.WriteLine("Müşteriler Listelendi");
+        }
+        public void Sil(Musteri musterii)
+        {
+            Console.WriteLine("Müşteriler silindi"+ musterii.Id+ musterii.Adi+ musterii.Soyadi);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri = new Musteri();
+            musteri.Adi = "aleyna";
+            musteri.Soyadi = "ilhan";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Adi = "berfin";
+            musteri2.Soyadi = "atar"; 
+
+            MusteriManager mn = new MusteriManager();
+            mn.Ekle(musteri);
+            mn.Ekle(musteri2);
+
+            Musteri[] musteriler = new Musteri[]
+            {
+                musteri,musteri2
+            };
+            mn.Listele(musteriler);
+            
+           
+           
+        }
+    }
+}
